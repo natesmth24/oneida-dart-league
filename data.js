09b57fd5ae825b978e755d1f3d36d9d2d2d55444
natesmth24 <natesmth24@gmail.com>
2026-09-22 21:00:19 -0400

    Add starter league data structure

---
+++ data.js
@@ -0,0 +1,19 @@
+// Temporary starter data. This file will later be generated from the league master workbook.
+const leagueData = {
+  standings: [
+    {team:"Pop-A-Top",wins:0,losses:0,gamesWon:0},
+    {team:"Elks",wins:0,losses:0,gamesWon:0},
+    {team:"Hi-Way Bowl 1",wins:0,losses:0,gamesWon:0},
+    {team:"Hi-Way Bowl 2",wins:0,losses:0,gamesWon:0},
+    {team:"Uncle J's",wins:0,losses:0,gamesWon:0},
+    {team:"Hotey",wins:0,losses:0,gamesWon:0}
+  ],
+  weeklyLeaders: {
+    "PPD":[], "MPR":[], "Hat Tricks":[], "High Tons":[], "White Horses":[]
+  },
+  upcoming: [
+    {home:"Pop-A-Top",away:"Elks"},
+    {home:"Hi-Way Bowl 1",away:"Hi-Way Bowl 2"},
+    {home:"Hotey",away:"Uncle J's"}
+  ]
+};
